25667b2a12108371e2b85378ea0b1d32257f7f12
Fidelis Yugita <fb46us@gmail.com>
2022-02-01 21:07:34 +0700

    add login & authenticate function

---
+++ functions/config.js
@@ -0,0 +1,14 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDxqhspIMQ0mJ_uopHCyR9MXWyGeEscD8A",
+  authDomain: "erp-firebase-4e1a2.firebaseapp.com",
+  databaseURL:
+    "https://erp-firebase-4e1a2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "erp-firebase-4e1a2",
+  storageBucket: "erp-firebase-4e1a2.appspot.com",
+  messagingSenderId: "254532011566",
+  appId: "erp-firebase-4e1a2",
+};
+
+module.exports = {
+  FIREBASE_CONFIG,
+};
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,83 @@
-const functions = require("firebase-functions");
+const config = require("./config");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const firebase = require("firebase/app");
+firebase.initializeApp(config.FIREBASE_CONFIG);
+const auth = require("@firebase/auth");
+
+const express = require("express");
+const app = express();
+
+exports.login = functions
+  .region("asia-southeast2")
+  .https.onRequest(async (req, res) => {
+    const { email, password } = req.body;
+    functions.logger.log(`LOGIN USING EMAIL: "${email}"`);
+
+    if (!email || !email.length > 0 || !password || !password.length > 0) {
+      return res.sendStatus(405);
+    }
+
+    try {
+      const userCredential = await auth.signInWithEmailAndPassword(
+        auth.getAuth(),
+        email,
+        password
+      );
+      return res.status(200).json(userCredential.user);
+    } catch (error) {
+      functions.logger.error(error.message);
+      return res.status(500).json({ message: error.message });
+    }
+  });
+
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const authenticate = async (req, res, next) => {
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith("Bearer ")
+  ) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+  const idToken = req.headers.authorization.split("Bearer ")[1];
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    functions.logger.log(`authenticate decodedIdToken:`, decodedIdToken);
+    req.user = decodedIdToken;
+    return next();
+  } catch (error) {
+    functions.logger.error(error.message);
+    return res.status(500).json({ message: "Invalid token" });
+  }
+};
+
+app.use(authenticate);
+
+// GET /message/{messageId}
+// Get details about a message
+app.get("/message/:messageId", async (req, res) => {
+  const messageId = req.params.messageId;
+  functions.logger.log(`LOOKING UP MESSAGE: "${messageId}"`);
+
+  try {
+    const uid = req.user.uid;
+    const snapshot = await admin
+      .database()
+      .ref(`/users/${uid}/messages/${messageId}`)
+      .once("value");
+
+    return res.status(200).json(snapshot.val());
+  } catch (error) {
+    functions.logger.error(error.message);
+    return res.sendStatus(500);
+  }
 });
+
+// Expose the API as a function
+exports.api = functions.region("asia-southeast2").https.onRequest(app);
