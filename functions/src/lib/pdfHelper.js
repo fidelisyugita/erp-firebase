94aa22252eb9075a50a1b6794c4e4e54b58f52af
Fidelis Yugita <fb46us@gmail.com>
2022-02-15 14:22:34 +0700

    resize

--- functions/src/lib/pdfHelper.js
+++ functions/src/lib/pdfHelper.js
@@ -20,7 +20,7 @@ exports.generatePdfProduct = (product, callback) => {
   const docDefinition = {
     content: [
       { text: `SKU: ${product.sku}`, style: "header" },
-      { image: canvas.toDataURL(), width: 200, height: 80 },
+      { image: canvas.toDataURL(), width: 300, height: 80 },
       { text: `Nama Produk: ${product.name}`, margin: [0, 10, 0, 0] },
       // { image: product.imageUrl, fit: [200, 200] },
       `Kategori: ${product?.category?.name}`,
@@ -28,6 +28,44 @@ exports.generatePdfProduct = (product, callback) => {
       { text: `Satuan: ${product?.measureUnit?.name}`, margin: [0, 10, 0, 0] },
       `Stock: ${product.stock}`,
       `Terjual: ${product.totalSold}`,
+    ],
+
+    styles: {
+      header: {
+        fontSize: 22,
+        bold: true,
+      },
+    },
+  };
+
+  const printer = new PdfPrinter(fontDescriptors);
+  const doc = printer.createPdfKitDocument(docDefinition);
+
+  let chunks = [];
+  doc.on("data", (chunk) => chunks.push(chunk));
+  doc.on("end", () => {
+    // const stringChunks = Buffer.concat(chunks).toString("base64");
+    // const base64 = `data:application/pdf;base64,${stringChunks}`;
+    // callback(Buffer.from(base64.split(",")[1], "base64"));
+
+    callback(Buffer.concat(chunks));
+  });
+  doc.end();
+};
+
+exports.generateDO = (transaction, callback) => {
+  const canvas = createCanvas(0, 0);
+  // create a bar code with the number/text I want and populate the canvas with it..
+  JsBarcode(canvas, transaction.id);
+
+  const docDefinition = {
+    content: [
+      { text: `Kode Invoice: ${transaction.invoiceCode}`, style: "header" },
+      { image: canvas.toDataURL(), width: 300, height: 80 },
+      {
+        text: `Note: ${transaction.note}`,
+        margin: [0, 10, 0, 0],
+      },
     ],
 
     styles: {
