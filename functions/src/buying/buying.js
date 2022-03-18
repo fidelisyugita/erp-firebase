c67345bc2e36b1261735f92b5363fce63da7aae7
Fidelis Yugita <fb46us@gmail.com>
2022-03-18 16:35:26 +0700

    add filter for transaction

--- functions/src/buying/buying.js
+++ functions/src/buying/buying.js
@@ -1,5 +1,6 @@
 const { logger } = require("firebase-functions");
 const { isNil, isEmpty } = require("ramda");
+const moment = require("moment");
 
 const { LIMIT_PER_PAGE, ERROR_MESSAGE } = require("../lib/config");
 const { authenticate } = require("../lib/authHelper");
@@ -25,21 +26,40 @@ app.use(authenticate);
 app.get("/", async (req, res) => {
   const keyword = String(req?.query?.keyword || "").toLowerCase();
 
+  const start = req?.query?.startDate
+    ? moment(req.query.startDate, "YYYY-MM-DD").toDate()
+    : null;
+  const end = req?.query?.endDate
+    ? moment(req.query.endDate, "YYYY-MM-DD").add(1, "d").toDate()
+    : null;
+  const statusId = String(req?.query?.statusId || "");
+  const typeId = String(req?.query?.typeId || "");
+
   const limit = Number(req?.query?.limit || LIMIT_PER_PAGE);
   const offset = req?.query?.page ? limit * Number(req.query.page) : 0;
   logger.log(
     `GET BUYINGS WITH KEYWORD: "${keyword}", LIMIT: "${limit}", OFFSET: "${offset}"`
   );
 
-  try {
-    const querySnapshot = await buyingsCollection
-      .where("isActive", "==", true)
+  let buyingRef = buyingsCollection.where("isActive", "==", true);
+  if (!isEmpty(statusId))
+    buyingRef = buyingRef.where("status.id", "==", statusId);
+  if (!isEmpty(typeId)) buyingRef = buyingRef.where("type.id", "==", typeId);
+
+  if (!isEmpty(keyword))
+    buyingRef = buyingRef
       .where("invoiceCodeLowercase", ">=", keyword)
       .where("invoiceCodeLowercase", "<=", keyword + "\uf8ff")
-      .orderBy("invoiceCodeLowercase")
-      .limit(limit)
-      .offset(offset)
-      .get();
+      .orderBy("invoiceCodeLowercase");
+  else {
+    if (start) buyingRef = buyingRef.where("createdAt", ">", start);
+    if (end) buyingRef = buyingRef.where("createdAt", "<", end);
+
+    buyingRef = buyingRef.orderBy("createdAt", "desc");
+  }
+
+  try {
+    const querySnapshot = await buyingRef.limit(limit).offset(offset).get();
     const result = querySnapshot.docs.map((doc) => {
       standarizeData(doc.data(), doc.id);
     });
--- functions/src/transaction/transaction.js
+++ functions/src/transaction/transaction.js
@@ -1,5 +1,6 @@
 const { logger } = require("firebase-functions");
 const { isNil, isEmpty } = require("ramda");
+const moment = require("moment");
 
 const { LIMIT_PER_PAGE, ERROR_MESSAGE } = require("../lib/config");
 const { authenticate } = require("../lib/authHelper");
@@ -25,18 +26,41 @@ app.use(authenticate);
 app.get("/", async (req, res) => {
   const keyword = String(req?.query?.keyword || "").toLowerCase();
 
+  const start = req?.query?.startDate
+    ? moment(req.query.startDate, "YYYY-MM-DD").toDate()
+    : null;
+  const end = req?.query?.endDate
+    ? moment(req.query.endDate, "YYYY-MM-DD").add(1, "d").toDate()
+    : null;
+  const statusId = String(req?.query?.statusId || "");
+  const typeId = String(req?.query?.typeId || "");
+
   const limit = Number(req?.query?.limit || LIMIT_PER_PAGE);
   const offset = req?.query?.page ? limit * Number(req.query.page) : 0;
   logger.log(
     `GET TRANSACTIONS WITH KEYWORD: "${keyword}", LIMIT: "${limit}", OFFSET: "${offset}"`
   );
 
-  try {
-    const querySnapshot = await transactionsCollection
-      .where("isActive", "==", true)
+  let transactionRef = transactionsCollection.where("isActive", "==", true);
+  if (!isEmpty(statusId))
+    transactionRef = transactionRef.where("status.id", "==", statusId);
+  if (!isEmpty(typeId))
+    transactionRef = transactionRef.where("type.id", "==", typeId);
+
+  if (!isEmpty(keyword))
+    transactionRef = transactionRef
       .where("invoiceCodeLowercase", ">=", keyword)
       .where("invoiceCodeLowercase", "<=", keyword + "\uf8ff")
-      .orderBy("invoiceCodeLowercase")
+      .orderBy("invoiceCodeLowercase");
+  else {
+    if (start) transactionRef = transactionRef.where("createdAt", ">", start);
+    if (end) transactionRef = transactionRef.where("createdAt", "<", end);
+
+    transactionRef = transactionRef.orderBy("createdAt", "desc");
+  }
+
+  try {
+    const querySnapshot = await transactionRef
       .limit(limit)
       .offset(offset)
       .get();
